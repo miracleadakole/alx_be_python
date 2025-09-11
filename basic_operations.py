6c4f6d91a892b6d171e749fd14e0657b4f724ed5
miracleadakole <109351488+miracleadakole@users.noreply.github.com>
2025-09-11 03:03:58 -0700

    Update basic_operations.py with arithmetic operations

--- basic_operations.py
+++ basic_operations.py
@@ -1,9 +1,11 @@
 number1 = 10
 number2 = 5
-result1 = number1 + number2
-result2 = number1 - number2
-result3 = number1 * number2
 
-print(f"Addition of {number1} and {number2} is {result1}")
-print(f"Subtraction of {number1} and {number2} is {result2}")
-print(f"Multiplication of {number1} and {number2} is {result3}")
+#Addition
+print(f"Addition of {number1} and {number2} is {number1 + number2}")
+
+#Subtraction
+print(f"Subtraction of {number1} and {number2} is {number1 - number2}")
+
+#Multiplication
+print(f"Multiplication of {number1} and {number2} is {number1 * number2}")
